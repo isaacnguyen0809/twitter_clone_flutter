2525c4e42473a30d7ed760fa0c258b2d89a68baa
SotaTek-KhanhNguyen2 <khanh.nguyen2@sotatek.com>
2023-04-04 18:00:53 +0700

    Initial setup

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppWriteConstants {
+  static const String databaseId = '642ba48e20201447afef';
+  static const String projectId = '642b9b85999ad2f84868';
+  static const String end = 'http://localhost:80/v1';
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'theme/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: AppTheme.theme,
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
+    );
+  }
+}
